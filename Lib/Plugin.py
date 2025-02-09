8ed56aff0270f9af7712ee8cba19fdd3b81b3ed6
christianhaitian <mckman@comcast.net>
2025-02-09 04:44:18 -0500

    Update Thumbnail Hashest thanks to lolage331
    Improved handling of inputstream thanks to lolage331

--- Lib/Plugin.py
+++ Lib/Plugin.py
@@ -4,6 +4,7 @@ from re import compile, findall, finditer, search, DOTALL, MULTILINE
 import sys, ssl
 from requests import Session, get, post, head, exceptions
 import six
+import inputstreamhelper
 import os #added by Christian Haitian
 from cloudscraper import * #added by Christian Haitian
 
@@ -2277,15 +2278,18 @@ def actionResolve(params):
         item = xbmcgui.ListItem(xbmc.getInfoLabel('ListItem.Label'))
         if flags['m3u8']:
 
-            item.setPath(urls['stream'])
+            is_helper = inputstreamhelper.Helper( 'hls' )
 
-            # Disable Kodi's MIME-type request, since we already know what it is.
-            item.setContentLookup(False)
-            item.setMimeType('application/x-mpegURL')
-            if KODI_VERSION < 19:
-                item.setProperty('inputstreamaddon', 'inputstream.adaptive')
+            if is_helper.check_inputstream():
+                item.setPath(urls['stream'])
+                # Disable Kodi's MIME-type request, since we already know what it is.
+                item.setContentLookup(False)
+                item.setMimeType('application/x-mpegURL')
+
+            if KODI_VERSION >= 19:
+                    item.setProperty('inputstream', is_helper.inputstream_addon)
             else:
-                item.setProperty('inputstream', 'inputstream.adaptive')
+                    item.setProperty('inputstreamaddon', is_helper.inputstream_addon)
 
             item.setProperty('inputstream.adaptive.stream_headers', '&'.join(key+'='+urllib_parse.quote_plus(val) for key, val in MEDIA_HEADERS.items()))
             item.setProperty('inputstream.adaptive.stream_params', '&'.join(key+'='+urllib_parse.quote_plus(val) for key, val in MEDIA_HEADERS.items()))
@@ -2295,12 +2299,13 @@ def actionResolve(params):
                 item.setProperty('inputstream.adaptive.manifest_type', 'hls')
             item.setProperty('inputstream.adaptive.original_audio_language', 'en')
 
-            playbackMethod = ADDON.getSetting('playbackMethod')            
+            playbackMethod = ADDON.getSetting('playbackMethod')
             if playbackMethod == '0':
                 item.setProperty('inputstream.adaptive.stream_selection_type', 'ask-quality')
             else:
                 item.setProperty('inputstream.adaptive.stream_selection_type', 'adaptive')
             #item.setProperty('inputstream.adaptive.config', '{"ssl_verify_peer":false}')
+
         else:
             item.setPath(urls['stream'] + '|' + '&'.join(key+'='+urllib_parse.quote_plus(val) for key, val in MEDIA_HEADERS.items()))
             if media_head:
